290a4f88c01c01dfda26447b032232a509fbbedf
KHutchens <48713734+Omnii-Analytics@users.noreply.github.com>
2023-08-22 14:38:10 +1000

    First Commit, contains writing 1 year of AFL data to SQL and fixture data

--- Initial_Collection.py
+++ Initial_Collection.py
@@ -1,55 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import psycopg2
+import os
+from dotenv import load_dotenv
 
-# Read the CSV containing match IDs into a dataframe
-match_ids_df = pd.read_csv('R23Fixture.csv')
+load_dotenv()
+
+connection = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+query = "SELECT * FROM fixture WHERE fixture_id BETWEEN 2 AND 23;"
+match_ids_df = pd.read_sql(query, connection)
+connection.close()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
 all_rows = []
-columns = None
 
 for _, row in match_ids_df.iterrows():
-    match_id = row['GameID']
-    round_number = row['round.roundNumber']
-    season_year = row['compSeason.year']
-    home_team = row['home.team.name']
-    away_team = row['away.team.name']
-    start_time = row['utcStartTime']
-
+    match_id = row['footywire_id']
     url = f"https://www.footywire.com/afl/footy/ft_match_statistics?mid={match_id}"
-    
+
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    for idx, table_selector in enumerate(["#match-statistics-team1-row table", "#match-statistics-team2-row table"]):
-        team_name = home_team if idx == 0 else away_team
-        opposing_team = away_team if idx == 0 else home_team
-        team_role = "Home" if idx == 0 else "Away"
-        
+    for table_selector in ["#match-statistics-team1-row table", "#match-statistics-team2-row table"]:
         tables = soup.select(table_selector)
         if tables and len(tables) > 1:
             target_table = tables[2]
-            for table_row in target_table.find_all('tr'):
+            for row_idx, table_row in enumerate(target_table.find_all('tr')):
+                if row_idx == 0:
+                    continue
+
                 values = [td.get_text(strip=True) for td in table_row.find_all('td')]
-                
-                # If it's the header row, update the columns list
-                if not columns:
-                    columns = ["Team", "Role", "Opposing Team", "Round Number", "Season Year", "Start Time"] + values
-                else:
-                    # Add team details before the actual data
-                    all_rows.append([team_name, team_role, opposing_team, round_number, season_year, start_time] + values)
+                all_rows.append([match_id] + values)
         else:
             print(f"Target table not found for match_id: {match_id} using selector {table_selector}.")
 
-# Convert all rows to a DataFrame at the end
-final_df = pd.DataFrame(all_rows, columns=columns)
-column_names = final_df.columns.tolist()
+final_df = pd.DataFrame(all_rows)
 
-print(final_df)
-print(column_names)
+# Create a dictionary to map footywire_id to fixture_id
+footywire_to_fixture_id = match_ids_df.set_index('footywire_id')['fixture_id'].to_dict()
+final_df['fixture_id'] = final_df[0].map(footywire_to_fixture_id)  # The first column temporarily holds the match_id
+final_df.drop(columns=[0], inplace=True)  # Remove the temporary match_id column
+
+player_ids = range(1, len(final_df) + 1)
+final_df.insert(0, 'playerid', player_ids)  # Add player ids
+
+# Manually set the columns
+final_df.columns = ['playerid', 'Player', 'K', 'HB', 'D', 'M', 'G', 'B', 'T', 'HO', 'GA', 'I50', 'CL', 'CG', 'R50', 'FF', 'FA', 'AF', 'SC', 'fixture_id']
+
+final_df.to_csv('temp.csv', index=False)
+
+# Connect to the database again
+connection = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+cursor = connection.cursor()
+with open('temp.csv', 'r') as f:
+    f.readline()  # skip the header
+    cursor.copy_from(f, 'playerstats', sep=',', null="")
+
+connection.commit()
+cursor.close()
+connection.close()
+
+print("Data inserted successfully!")
